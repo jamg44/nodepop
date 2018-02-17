e91242d2796097a7875a739006c42f5d59863f09
Javi Miguel <jamg44@gmail.com>
2018-02-17 11:53:32 +0100

    load fs lib, connect to database, fix json, remove before load, call main function

--- nodepop/install_db.js
+++ nodepop/install_db.js
@@ -1,11 +1,27 @@
-const anuncio = require("./Models/Anuncio");
-const anuncios = JSON.parse(
+// no habias cargado la librería fs
+const fs = require('fs');
+
+// no habias conectado a la base de datos
+require("./lib/connectMongoose");
+
+const Anuncio = require("./Models/Anuncio");
+const json = JSON.parse(
   fs.readFileSync(__dirname + "/anuncios.json", "utf-8")
 );
 
-async function loadMeetings() {
+// no estabas llamando a la función loadMeetings()
+loadMeetings(json.anuncios).catch(err => console.log(err));
+
+async function loadMeetings(anuncios) {
   try {
-    await anuncio.insertMany(anuncios);
+
+    // antes de cargarlos vaciamos la colección
+    await Anuncio.remove();
+    console.log('Anuncios borrados.')
+
+    await Anuncio.insertMany(anuncios);
+    console.log('Anuncios cargados.')
+
     console.log("Done!");
     process.exit();
   } catch (e) {
